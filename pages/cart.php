badbae21844b0279eb8b07f5d685732692800eba
Shawn Ning <shawnningjiale@gmail.com>
2023-06-13 19:05:24 +0800

    Update

--- pages/cart.php
+++ pages/cart.php
@@ -4,25 +4,82 @@ require "parts/header.php";
 require "parts/navbar.php";
 
 ?>
+<div class="container mt-5 mb-2 mx-auto" style="max-width: 900px">
+      <div class="min-vh-100">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <h1 class="h1 text-white"><?php echo $_SESSION['user']['name'] ?>'s cart</h1>
+        </div>
 
-<h1>Cart Page</h1>
-    <table>
-        <tr>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Actions</th>
-        </tr>
-        <?php foreach ($_SESSION['cart'] as $product_id => $quantity): ?>
-        <tr>
-            <td>Product <?php echo $product_id; ?></td>
-            <td><?php echo $quantity; ?></td>
-            <td>
-                <a href="remove.php">Remove</a>
-                <a href="update.php?product_id=<?php echo $product_id; ?>">Update Quantity</ a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
+        <!-- List of products user added to cart -->
+        <table
+          class="table table-hover table-bordered table-striped table-dark border-white"
+        >
+          <thead>
+            <tr>
+              <th scope="col">Product</th>
+              <th scope="col">Price</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Total</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+            <?php if ( empty( cart() ) ) : ?>
+              <tr>
+                <td colspan="5">Your cart is empty.</td>
+              </tr>
+            <?php else : ?>
+              <?php foreach( Cart::listAllProductsinCart() as $product ) : ?>
+                <tr>
+                  <td><?php echo $product['name']; ?></td>
+                  <td>RM <?php echo $product['price']; ?></td>
+                  <td><?php echo $product['quantity']; ?></td>
+                  <td>RM <?php echo $product['total']; ?></td>
+                  <td>
+                    <form
+                      method="POST"
+                      action="cart/remove"
+                    >
+                      <input 
+                        type="hidden" 
+                        name="action" 
+                        value="remove" 
+                      />
+                      <input 
+                        type="hidden"
+                        name="product_id"
+                        value="<?php echo $product['id']; ?>"
+                      />
+                      <button class="btn btn-danger btn-sm">
+                        <i class="bi bi-trash"></i>
+                      </button>
+                    </form>
+                  </td>
+                </tr>
+                <?php endforeach; ?>
+                <tr>
+                  <td colspan="3" class="text-end">Total</td>
+                  <td colspan="2">$ <?php echo Cart::total(); ?></td>
+                </tr>
+            <?php endif; ?>
+          </tbody>
+        </table>
+
+        <div class="d-flex justify-content-between align-items-center my-3">
+          <a href="/product" class="btn btn-secondary btn-sm"
+            >Continue Shopping</a
+          >
+          <?php if ( !empty( Cart::listAllProductsinCart() ) ) : ?>
+            <form 
+              method="POST"
+              action="/checkout"
+            >
+              <button class="btn btn-light">Checkout</a>
+            </form>
+          </div>
+            <?php endif; ?>
+        </div>
+      </div>
 
 <?php
 
